e32f9ea15b4ddfac5162ee78a21d6a2d3b4ed4e9
mouradaze <mouradaze10@gmail.com>
2023-11-03 19:17:24 +0100

    Exercice 4 : ajout des pages jsf et le bean d'affichage petit modification au niveau de affichage de l'exercice 3

---
+++ src/main/java/jsf/AffichageBean.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "affichage")
+@RequestScoped
+public class AffichageBean {
+    private int nombre;
+    public int getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(int n){
+        this.nombre = n;
+    }
+    public List<Integer> getNombresSuivants(){
+        List<Integer> res = new ArrayList<Integer>(5);
+        for(int i=nombre;i<nombre+5;i++){
+            res.add(i);
+        }
+        return res;
+    }
+    public String afficher(){
+        return "affichage_4?nb="+nombre+"&amp;faces-redirect=true";
+    }
+
+    /**
+     * Creates a new instance of bean
+     */
+    public AffichageBean() {
+    }
+    
+}
