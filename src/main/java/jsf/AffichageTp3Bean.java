869874d05b0049b9fa09d83d87363aebf4ab5fa8
mouradaze <mouradaze10@gmail.com>
2023-11-05 13:36:45 +0100

    redéfinissement du 4eme exercice dans la template

---
+++ src/main/java/jsf/AffichageTp3Bean.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "affichageTp3Bean")
+@RequestScoped
+public class AffichageTp3Bean {
+    private int nombre;
+    public int getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(int n){
+        this.nombre = n;
+    }
+    public List<Integer> getNombresSuivants(){
+        List<Integer> res = new ArrayList<Integer>(5);
+        for(int i=nombre;i<nombre+5;i++){
+            res.add(i);
+        }
+        return res;
+    }
+    public String afficher(){
+        return "affichage__4?nb="+nombre+"&amp;faces-redirect=true";
+    }
+
+    /**
+     * Creates a new instance of AffichageTp3Bean
+     */
+    public AffichageTp3Bean() {
+    }
+    
+}
